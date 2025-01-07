aeec5f9935f2618eb38e764726f7b19d3439651b
tommaso.panciroli <tommypanci@gmail.com>
2025-01-07 15:39:47 +0100

    finished exercise 1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,38 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+
+
+        System.out.print(Main.concat("Ciao sono Tommaso e sono nato nel", 2003));
+        System.out.print(Main.inserisciInArray(new String[]{"Tommaso", "Valerio", "Valentina", "Martino", "Francesco"}, "Ciaone ragazzi"));
+        System.out.print(Main.moltiplicazione(6, 8));
+        ;
+    }
+
+
+    public static int moltiplicazione(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static String[] inserisciInArray(String[] nomi, String str) {
+        if (nomi.length != 5) throw new Error("Errore dell'array");
+        String[] array = new String[6];
+        array[0] = nomi[0];
+        array[1] = nomi[1];
+        array[2] = str;
+        array[3] = nomi[3];
+        array[4] = nomi[4];
+        array[5] = nomi[5];
+        return array;
+
+    }
+
+
+    public static String concat(String str, int num) {
+        return str + " " + num;
     }
 }
+
+
+
+
